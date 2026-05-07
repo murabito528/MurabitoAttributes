fb2621faa4bfbc398ac0b5cb760be4d19dbc5fac
murabito <tatsuharu.sumiyoshi@gmail.com>
2026-05-07 19:36:43 +0900

    gui関連の呼び出しをクライアントのみにしていなかったのを修正

--- src/main/java/com/murabito/murabitoattributesmod/events/RenderEvent.java
+++ src/main/java/com/murabito/murabitoattributesmod/events/RenderEvent.java
@@ -9,13 +9,14 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.inventory.Slot;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = MurabitoAttributesMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid = MurabitoAttributesMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class RenderEvent {
     @SubscribeEvent
     public static void onRenderGuiPost(ScreenEvent.Render.Post event) {
